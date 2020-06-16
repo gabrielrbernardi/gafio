338ba1a567ae401b1479ca9633db523e18436e9a
Bruno Oliveira Sinhoroto <bruno.sinhoroto@hotmail.com>
2020-06-16 19:52:18 -0300

    Bug fixed on table History

--- server/src/database/migrations/04_createHistoryTable.ts
+++ server/src/database/migrations/04_createHistoryTable.ts
@@ -1,17 +1,16 @@
 import knex from 'knex';
 
 export async function up(knex: knex){
-    return knex.schema.createTable('Usuario', table => {
+    return knex.schema.createTable('Historico', table => {
         table.increments('IdHistorico').primary();
-        table.string('NroProntuario').notNullable();
+        table.string('NroProntuario').notNullable().references('NroProntuario').inTable('Prontuario');
         table.string('NomePaciente').notNullable();
         table.date('DataNascimento').notNullable();
         table.enu('Genero', ['M', 'F']);
         table.timestamps(true, true);
-        table.integer('NroProntuario').notNullable().references('NroProntuario').inTable('Prontuario');
     });
 }
 
 export async function down(knex: knex){
-    return knex.schema.dropTable('usuario');
+    return knex.schema.dropTable('Historico');
 }
