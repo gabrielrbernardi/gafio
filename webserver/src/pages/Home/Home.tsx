c3d80813c93d3be1e18a408cdac6a377fd97260a
Joao Victor <jvsfernandes924@gmail.com>
2020-10-23 14:15:36 -0300

    Microbiology page- frontend    added toast and loandig components.    added link to the page

--- webserver/src/pages/Home/Home.tsx
+++ webserver/src/pages/Home/Home.tsx
@@ -15,58 +15,82 @@ const Home = () => {
             <div className="m-5 p-3 card-columns">
                 <Link className="text-decoration-none" to="/medicalRecords">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <RiPagesLine className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>PRONTUÁRIOS</strong>
-                            </p>
+                        <p className="h6">
+                            <RiPagesLine
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>PRONTUÁRIOS</strong>
+                        </p>
                     </div>
                 </Link>
                 <Link className="text-decoration-none" to="/registrations">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <RiComputerLine className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>CADASTROS</strong>
-                            </p>
+                        <p className="h6">
+                            <RiComputerLine
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>CADASTROS</strong>
+                        </p>
                     </div>
                 </Link>
                 <Link className="text-decoration-none" to="/dashboard">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <RiBarChart2Line className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>DASHBOARD</strong>
-                            </p>
+                        <p className="h6">
+                            <RiBarChart2Line
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>DASHBOARD</strong>
+                        </p>
                     </div>
                 </Link>
             </div>
 
             <div className="m-5 p-3 card-columns justify-content-center">
-                <Link className="text-decoration-none" to="/underDevelopment">
+                <Link className="text-decoration-none" to="/microbiology">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <FaMicroscope className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>MICROBIOLOGIA</strong>
-                            </p>
+                        <p className="h6">
+                            <FaMicroscope
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>MICROBIOLOGIA</strong>
+                        </p>
                     </div>
                 </Link>
                 <Link className="text-decoration-none" to="/underDevelopment">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <FaHospital className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>RECOMENDAÇÕES CCIH</strong>
-                            </p>
+                        <p className="h6">
+                            <FaHospital
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>RECOMENDAÇÕES CCIH</strong>
+                        </p>
                     </div>
                 </Link>
                 <Link className="text-decoration-none" to="/history">
                     <div className="card text-center shadow zoom-hover">
-                            <p className="h6">
-                                <FaHistory className="mt-3 mb-3 ml-auto mr-auto" size={40} /><br/>
-                                <strong>HISTÓRICO</strong>
-                            </p>
+                        <p className="h6">
+                            <FaHistory
+                                className="mt-3 mb-3 ml-auto mr-auto"
+                                size={40}
+                            />
+                            <br />
+                            <strong>HISTÓRICO</strong>
+                        </p>
                     </div>
                 </Link>
             </div>
         </>
-    )
+    );
 }
 
 export default Home;
--- webserver/src/pages/Microbiology/index.tsx
+++ webserver/src/pages/Microbiology/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 import ToastComponent from "../../components/Toast";
+import Loading from "../../components/Loading";
 
 import api from "../../services/api";
 
@@ -33,6 +35,14 @@ const Microbiology = () => {
     const [records, setRecords] = useState<number>(0);
     const [first, setFirst] = useState<number>(0);
 
+    const [toast, setToast] = useState<boolean>(false);
+    const [getMessageType, setMessageType] = useState<string>("");
+    const [getMessageTitle, setMessageTitle] = useState<string>("");
+    const [getMessageContent, setMessageContent] = useState<string>("");
+
+    const [tableloading, setTableLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(true);
+
     const rows = 10;
 
     useEffect(() => {
@@ -44,7 +54,11 @@ const Microbiology = () => {
                 const records = Number(count);
                 setRecords(records);
             } catch (error) {
-                //  toast.error("Falha ao carregar dados");
+                HandleToast(
+                    "error",
+                    "Erro!",
+                    "Falha ao carregar os registros."
+                );
             }
         }
 
@@ -56,8 +70,16 @@ const Microbiology = () => {
                 );
                 const { data } = response;
                 setMicrobiologies(data);
+                setLoading(false);
+                setTableLoading(false);
             } catch (error) {
-                //  toast.error("Falha ao carregar dados");
+                setLoading(false);
+                setTableLoading(false);
+                HandleToast(
+                    "error",
+                    "Erro!",
+                    "Falha ao carregar os registros."
+                );
             }
         }
 
@@ -67,18 +89,35 @@ const Microbiology = () => {
 
     async function handlePage(event: any) {
         try {
-             const index = event.first;
-             const page = Number(index) / 10 + 1;
-             const response = await api.get("microbiology", {
-                 params: { page },
-             });
-             const { data } = response;
-             setMicrobiologies(data);
-             setFirst(index);
+            setTableLoading(true);
+            const index = event.first;
+            const page = Number(index) / 10 + 1;
+            const response = await api.get("microbiology", {
+                params: { page },
+            });
+            const { data } = response;
+            setMicrobiologies(data);
+            setFirst(index);
+            setTableLoading(false);
         } catch (error) {
-         //toast de erro   
-         console.log(error)
+            setTableLoading(false);
+            HandleToast("error", "Erro!", "Falha ao carregar os registros.");
         }
+    }
+
+    function HandleToast(
+        messageType: string,
+        messageTitle: string,
+        messageContent: string
+    ) {
+        setToast(false);
+        setMessageType(messageType);
+        setMessageTitle(messageTitle);
+        setMessageContent(messageContent);
+        setToast(true);
+        setTimeout(() => {
+            setToast(false);
+        }, 4500);
     }
 
     const header = (
@@ -90,100 +129,110 @@ const Microbiology = () => {
     );
 
     return (
-        <div className="row m-5 px-5">
-            {first}
-            <Button
-                variant="outline-dark"
-                className="mb-2"
-                style={{ borderRadius: "0", height: "41.5px" }}
-            >
-                Cadastrar Microbiologia
-            </Button>
-            <Button
-                variant="outline-secondary"
-                className="mb-2 ml-2"
-                aria-controls="example-collapse-text"
-                style={{ borderRadius: "0" }}
-            >
-                Buscar por microbiologia
-            </Button>
+        <>
+            <div className="row m-5 px-5">
+                <Button
+                    variant="outline-dark"
+                    className="mb-2"
+                    style={{ borderRadius: "0", height: "41.5px" }}
+                >
+                    Cadastrar Microbiologia
+                </Button>
+                <Button
+                    variant="outline-secondary"
+                    className="mb-2 ml-2"
+                    aria-controls="example-collapse-text"
+                    style={{ borderRadius: "0" }}
+                >
+                    Buscar por microbiologia
+                </Button>
 
-            <DataTable
-                value={microbiologies}
-                paginator={true}
-                rows={rows}
-                totalRecords={records}
-                first={first}
-                onPage={handlePage}
-                header={header}
-                emptyMessage="Nenhum resultado encontrado"
-                lazy={true}
-                selectionMode="single"
-                className="p-datatable-responsive-demo"
-                resizableColumns={true}
-            >
-                <Column
-                    field="IdMicrobiologia"
-                    header="Id"
-                    style={{ width: "6%", textAlign: "center" }}
+                <DataTable
+                    value={microbiologies}
+                    loading={tableloading}
+                    paginator={true}
+                    rows={rows}
+                    totalRecords={records}
+                    first={first}
+                    onPage={handlePage}
+                    header={header}
+                    emptyMessage="Nenhum resultado encontrado"
+                    lazy={true}
+                    selectionMode="single"
+                    className="p-datatable-responsive-demo"
+                    resizableColumns={true}
+                >
+                    <Column
+                        field="IdMicrobiologia"
+                        header="Id"
+                        style={{ width: "6%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="IdPaciente"
+                        header="Paciente"
+                        style={{ width: "8%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="IdProntuario"
+                        header="Prontuário"
+                        style={{ width: "8%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="DataColeta"
+                        header="Coleta"
+                        style={{ width: "10%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="DataResultado"
+                        header="Resultado"
+                        style={{ width: "10%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="SwabNasal"
+                        header="Swab nasal"
+                        style={{ width: "7%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="SwabRetal"
+                        header="Swab retal"
+                        style={{ width: "7%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="Sangue"
+                        header="Sangue"
+                        style={{ width: "6%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="Urina"
+                        header="Urina"
+                        style={{ width: "6%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="SecrecaoTraqueal"
+                        header="Secreção Traqueal"
+                        style={{ width: "8%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="Outros"
+                        header="Outros"
+                        style={{ width: "6%", textAlign: "center" }}
+                    />
+                    <Column
+                        field="PerfilSensibilidade"
+                        header="Perfil"
+                        style={{ width: "12%", textAlign: "center" }}
+                    />
+                </DataTable>
+            </div>
+            {toast && (
+                <ToastComponent
+                    messageType={getMessageType}
+                    messageTitle={getMessageTitle}
+                    messageContent={getMessageContent}
                 />
-                <Column
-                    field="IdPaciente"
-                    header="Paciente"
-                    style={{ width: "8%", textAlign: "center" }}
-                />
-                <Column
-                    field="IdProntuario"
-                    header="Prontuário"
-                    style={{ width: "8%", textAlign: "center" }}
-                />
-                <Column
-                    field="DataColeta"
-                    header="Coleta"
-                    style={{ width: "10%", textAlign: "center" }}
-                />
-                <Column
-                    field="DataResultado"
-                    header="Resultado"
-                    style={{ width: "10%", textAlign: "center" }}
-                />
-                <Column
-                    field="SwabNasal"
-                    header="Swab nasal"
-                    style={{ width: "7%", textAlign: "center" }}
-                />
-                <Column
-                    field="SwabRetal"
-                    header="Swab retal"
-                    style={{ width: "7%", textAlign: "center" }}
-                />
-                <Column
-                    field="Sangue"
-                    header="Sangue"
-                    style={{ width: "6%", textAlign: "center" }}
-                />
-                <Column
-                    field="Urina"
-                    header="Urina"
-                    style={{ width: "6%", textAlign: "center" }}
-                />
-                <Column
-                    field="SecrecaoTraqueal"
-                    header="Secreção Traqueal"
-                    style={{ width: "8%", textAlign: "center" }}
-                />
-                <Column
-                    field="Outros"
-                    header="Outros"
-                    style={{ width: "6%", textAlign: "center" }}
-                />
-                <Column
-                    field="PerfilSensibilidade"
-                    header="Perfil"
-                    style={{ width: "12%", textAlign: "center" }}
-                />
-            </DataTable>
-        </div>
+            )}
+            {loading && <Loading />}
+        </>
     );
 };
 
