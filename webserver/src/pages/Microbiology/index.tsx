e6bebd5ec22a2c4ebb845dc7b3c317a94e8537e8
Joao Victor <jvsfernandes924@gmail.com>
2020-11-10 14:44:50 -0300

    Microbiology frontend - show by filter

--- webserver/src/pages/Microbiology/index.tsx
+++ webserver/src/pages/Microbiology/index.tsx
@@ -9,6 +9,11 @@ import Loading from "../../components/Loading";
 import { Dialog } from "primereact/dialog";
 import View from "./MicrobiologyView";
 import Form from "./MicrobiologyForm";
+import { Dropdown } from "primereact/dropdown";
+import { InputText } from "primereact/inputtext";
+import Collapse from "react-bootstrap/Collapse";
+import { FiSearch } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
 
 import "./index.css";
 
@@ -43,7 +48,7 @@ const Microbiology = () => {
     const [view, setView] = useState<boolean>(false);
     const [update, setUpdate] = useState<boolean>(false);
     const [deleteDialog, setDeleteDialog] = useState<boolean>(false);
-    const [displayDialog, setDisplayDialog] = useState(false);
+    const [displayDialog, setDisplayDialog] = useState<boolean>(false);
     const [selectedMicrobiology, setselectedMicrobiology] = useState<any>(null);
 
     const [toast, setToast] = useState<boolean>(false);
@@ -54,8 +59,21 @@ const Microbiology = () => {
     const [tableloading, setTableLoading] = useState<boolean>(true);
     const [loading, setLoading] = useState<boolean>(true);
 
+    const [open, setOpen] = useState<boolean>(false);
+    const [optionState, setOptionState] = useState<any>(null);
+    const [filter, setFilter] = useState<string>("");
+    const [filterValue, setFilterValue] = useState<any>("");
+    const [successfulSearch, setSuccessfulSearch] = useState<boolean>(false);
+
     const rows = 10;
 
+    const dropdownOptions = [
+        { name: "Id", filter: "id" },
+        { name: "Paciente", filter: "paciente" },
+        { name: "Prontuário", filter: "prontuario" },
+        { name: "Data da coleta", filter: "dataColeta" },
+        { name: "Data do Resultado", filter: "dataResultado" },
+    ];
     useEffect(() => {
         //carrega os dados da tabela e a quantidade total de registros
         async function loadMicrobiologies() {
@@ -81,15 +99,38 @@ const Microbiology = () => {
         loadMicrobiologies();
     }, []);
 
+    // Toast
+    function HandleToast(
+        messageType: string,
+        messageTitle: string,
+        messageContent: string
+    ) {
+        setToast(false);
+        setMessageType(messageType);
+        setMessageTitle(messageTitle);
+        setMessageContent(messageContent);
+        setToast(true);
+        setTimeout(() => {
+            setToast(false);
+        }, 4500);
+    }
+
     //Busca os registros por página
     async function handlePage(event: any) {
         try {
-            setTableLoading(true);
             const index = event.first;
             const page = Number(index) / 10 + 1;
-            const response = await api.get("microbiology", {
-                params: { page },
-            });
+            let response;
+            if (filter && successfulSearch) {
+                response = await api.get("microbiology", {
+                    params: { page, filter, filterValue },
+                });
+            } else {
+                response = await api.get("microbiology", {
+                    params: { page },
+                });
+            }
+            setTableLoading(true);
             const { results } = response.data;
             setMicrobiologies(results);
             setFirst(index);
@@ -99,7 +140,7 @@ const Microbiology = () => {
             HandleToast("error", "Erro!", "Falha ao carregar os registros.");
         }
     }
-   
+
     function onMicrobiologySelect(e: any) {
         const microbiologyData = e.data;
         const { IdMicrobiologia } = microbiologyData;
@@ -111,7 +152,8 @@ const Microbiology = () => {
     async function handleTableUpdate() {
         try {
             setTableLoading(true);
-            const response = await api.get("/microbiology");
+            let response;
+            response = await api.get("microbiology");
             const { results } = response.data;
             const { count } = response.data.count;
             setRecords(Number(count));
@@ -141,22 +183,50 @@ const Microbiology = () => {
         handleTableUpdate();
     }
 
-    function HandleToast(
-        messageType: string,
-        messageTitle: string,
-        messageContent: string
-    ) {
-        setToast(false);
-        setMessageType(messageType);
-        setMessageTitle(messageTitle);
-        setMessageContent(messageContent);
-        setToast(true);
-        setTimeout(() => {
-            setToast(false);
-        }, 4500);
+    //Para filtragem
+    async function handleSearch() {
+        if (!optionState) {
+            HandleToast("error", "Erro!", "Selecione um filtro para buscar.");
+            return;
+        }
+        if (!filterValue) {
+            HandleToast("error", "Erro!", "Digite algum valor para pesquisar.");
+            return;
+        }
+        setLoading(true);
+        setTableLoading(true);
+        try {
+            const response = await api.get("/microbiology", {
+                params: { filter, filterValue },
+            });
+            const { results } = response.data;
+            const { count } = response.data.count;
+            setRecords(Number(count));
+            setMicrobiologies(results);
+            setLoading(false);
+            setTableLoading(false);
+            let res = "resultado";
+            if (count > 1) res += "s";
+            HandleToast(
+                "info",
+                "Resultado Encontrado!",
+                `Foi encontrado ${count} ${res}.`
+            );
+            setSuccessfulSearch(true);
+        } catch (err) {
+            setLoading(false);
+            setTableLoading(false);
+            const message = err.response.data.error;
+            HandleToast("error", "Sem resultado!", `${message}`);
+        }
     }
 
-    // Para as colunas da tabela 
+    function onOptionChange(e: { value: any }) {
+        setOptionState(e.value);
+        setFilter(e.value.filter);
+    }
+
+    // Para as colunas da tabela
     const IdBodyTemplate = (rowData: any) => {
         return (
             <React.Fragment>
@@ -256,15 +326,6 @@ const Microbiology = () => {
         );
     };
 
-    const PerfilSensibilidadeTemplate = (rowData: any) => {
-        return (
-            <React.Fragment>
-                <span className="p-column-title">Perfil</span>
-                <a>{rowData.PerfilSensibilidade}</a>
-            </React.Fragment>
-        );
-    };
-
     return (
         <>
             <div className="row m-5 px-5">
@@ -281,10 +342,95 @@ const Microbiology = () => {
                     variant="outline-secondary"
                     className="mb-2 ml-2"
                     aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                    onClick={() => setOpen(!open)}
                     style={{ borderRadius: "0" }}
                 >
                     Buscar por microbiologia
                 </Button>
+
+                <Collapse in={open} timeout={200}>
+                    <div className="ml-2">
+                        <div className="p-inputgroup">
+                            <span className="p-float-label">
+                                <InputText
+                                    id="float-input"
+                                    type="search"
+                                    value={filterValue}
+                                    onChange={(e) => {
+                                        setFilterValue(
+                                            (e.target as HTMLInputElement).value
+                                        );
+                                        if((e.target as HTMLInputElement).value === "" && successfulSearch){
+                                            setFilterValue("");
+                                            setOptionState(null);
+                                            setSuccessfulSearch(false);
+                                            handleTableUpdate();
+                                        }
+                                    }}
+                                    onKeyPress={(ev) => {
+                                        if (ev.key === "Enter") {
+                                            handleSearch();
+                                            ev.preventDefault();
+                                        }
+                                    }}
+                                    style={{
+                                        minWidth: "4em",
+                                        borderRadius: "0",
+                                    }}
+                                    size={30}
+                                />
+                                {optionState === null ? (
+                                    <label htmlFor="float-input">Buscar</label>
+                                ) : (
+                                    <label htmlFor="float-input">
+                                        Buscar por {optionState.name}
+                                    </label>
+                                )}
+                            </span>
+                            {filterValue && (
+                                <>
+                                    <Dropdown
+                                        className="mx-1"
+                                        value={optionState}
+                                        options={dropdownOptions}
+                                        onChange={onOptionChange}
+                                        placeholder="Selecione um filtro"
+                                        optionLabel="name"
+                                        style={{ width: "12em" }}
+                                    />
+                                    <Button
+                                        tabIndex={2}
+                                        variant="outline-danger"
+                                        className="p-0 mr-1"
+                                        style={{
+                                            width: "17px",
+                                            borderRadius: "0",
+                                        }}
+                                        onClick={() => {
+                                            setFilterValue("");
+                                            setOptionState(null);
+                                            setSuccessfulSearch(false);
+                                            handleTableUpdate();
+                                        }}
+                                    >
+                                        <AiOutlineClose size={15} />
+                                    </Button>
+                                    <Button
+                                        onClick={handleSearch}
+                                        style={{
+                                            borderRadius: "0",
+                                        }}
+                                    >
+                                        <FiSearch size={15} />
+                                    </Button>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </Collapse>
+                <div className="ml-auto"></div>
+
                 <div className="datatable-responsive-demo">
                     <DataTable
                         value={microbiologies}
@@ -308,28 +454,67 @@ const Microbiology = () => {
                             onMicrobiologySelect(e);
                         }}
                     >
-                        <Column field="IdMicrobiologia" header="Id" body={IdBodyTemplate}/>
-                        <Column field="IdPaciente" header="Paciente" body={PacienteBodyTemplate} />
-                        <Column field="IdProntuario" header="Prontuário" body={ProntuarioBodyTemplate} />
-                        <Column field="DataColeta" header="Coleta" body={DataColetaBodyTemplate}/>
-                        <Column field="DataResultado" header="Resultado" body={DataResultadoBodyTemplate} />
-                        <Column field="SwabNasal" header="Swab Nasal" body={SwabNasalBodyTemplate} />
-                        <Column field="SwabRetal" header="Swab Retal" body={SwabRetalBodyTemplate}/>
-                        <Column field="Sangue" header="Sangue" body={SangueBodyTemplate}/>
-                        <Column field="Urina" header="Urina" body={UrinaBodyTemplate} />
+                        <Column
+                            field="IdMicrobiologia"
+                            header="Id"
+                            body={IdBodyTemplate}
+                        />
+                        <Column
+                            field="IdPaciente"
+                            header="Paciente"
+                            body={PacienteBodyTemplate}
+                        />
+                        <Column
+                            field="IdProntuario"
+                            header="Prontuário"
+                            body={ProntuarioBodyTemplate}
+                        />
+                        <Column
+                            field="DataColeta"
+                            header="Coleta"
+                            body={DataColetaBodyTemplate}
+                        />
+                        <Column
+                            field="DataResultado"
+                            header="Resultado"
+                            body={DataResultadoBodyTemplate}
+                        />
+                        <Column
+                            field="SwabNasal"
+                            header="Swab Nasal"
+                            body={SwabNasalBodyTemplate}
+                        />
+                        <Column
+                            field="SwabRetal"
+                            header="Swab Retal"
+                            body={SwabRetalBodyTemplate}
+                        />
+                        <Column
+                            field="Sangue"
+                            header="Sangue"
+                            body={SangueBodyTemplate}
+                        />
+                        <Column
+                            field="Urina"
+                            header="Urina"
+                            body={UrinaBodyTemplate}
+                        />
                         <Column
                             field="SecrecaoTraqueal"
                             header="Secreção Traqueal"
                             body={SecrecaoTraquealBodyTemplate}
                         />
-                        <Column field="Outros" header="Outros" body={OutrosBodyTemplate}/>
-                        <Column field="PerfilSensibilidade" header="Perfil" body={PerfilSensibilidadeTemplate}/>
+                        <Column
+                            field="Outros"
+                            header="Outros"
+                            body={OutrosBodyTemplate}
+                        />
                     </DataTable>
                 </div>
 
                 <Dialog
                     visible={displayDialog}
-                    style={{ width: "50%" }}
+                    style={{ width: "55%" }}
                     header="Ações"
                     modal={true}
                     onHide={() => setDisplayDialog(false)}
