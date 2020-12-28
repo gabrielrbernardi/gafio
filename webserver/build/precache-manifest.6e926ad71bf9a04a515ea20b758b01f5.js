8db20f9e54df4364ab2472b04c153535cdad9c38
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2020-12-28 15:31:18 -0300

    Deployment files

---
+++ webserver/build/precache-manifest.6e926ad71bf9a04a515ea20b758b01f5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c378c3a0ef437ac5ca311b3b5e4b1899",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cec6244be210bab6e6",
+    "url": "/static/css/2.e9df2a46.chunk.css"
+  },
+  {
+    "revision": "f3b7a8c448d6fc68f1d7",
+    "url": "/static/css/main.1f1d218e.chunk.css"
+  },
+  {
+    "revision": "96cec6244be210bab6e6",
+    "url": "/static/js/2.7cc1b440.chunk.js"
+  },
+  {
+    "revision": "9469d00b6a7f1e021b4b75983fa6140a",
+    "url": "/static/js/2.7cc1b440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3b7a8c448d6fc68f1d7",
+    "url": "/static/js/main.252a9f3c.chunk.js"
+  },
+  {
+    "revision": "d4c1dfb0c2271ce35cdc",
+    "url": "/static/js/runtime-main.6934f41e.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "9a56b3de6cc2c1d66d36acf5e616ae49",
+    "url": "/static/media/fiocruzBanner.9a56b3de.jpg"
+  },
+  {
+    "revision": "afbb0d3300d47154d98bfd6324ba2090",
+    "url": "/static/media/logoFiocruz.afbb0d33.png"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
