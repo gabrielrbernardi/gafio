ccbf1be3e9cdc2aeef0d15ccf07e2a203004f1ac
Gabriel Bernardi <gabrielrbernardi@gmail.com>
2021-05-27 11:17:51 -0300

    Bug Fix on migration

--- server/src/database/migrations/11_addingUserType.ts
+++ server/src/database/migrations/11_addingUserType.ts
@@ -1,9 +1,9 @@
 import knex from 'knex';
 
 export async function up(knex: knex) {
-    return knex.schema.raw("alter table usuario modify column TipoUsuario enum('A', 'M', 'F', 'L');");
+    return knex.schema.raw("ALTER TABLE Usuario MODIFY COLUMN TipoUsuario ENUM('A', 'M', 'F', 'L');");
 }
 
 export async function down(knex: knex) {
-    return knex.schema.raw("alter table usuario modify column TipoUsuario enum('A', 'M', 'F', 'L');");
+    return knex.schema.raw("ALTER TABLE usuario MODIFY COLUMN TipoUsuario ENUM('A', 'M', 'F', 'L');");
 }
